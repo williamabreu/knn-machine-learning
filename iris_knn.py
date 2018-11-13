c5653b7aac106b35fdad56213d5ac1cbebbdd65a
williamabreu <williamsantos_00@hotmail.com>
2018-11-13 15:39:23 -0200

    Fazendo leitura do arquivo do meu jeito
    Obtém o cabeçalho e monta a matriz dos dados de forma escalável,
    independente da dimensão.
    
    Ainda não testado.

--- iris_knn.py
+++ iris_knn.py
@@ -1,76 +1,77 @@
 # Example of kNN implemented from Scratch in Python
 
-import csv
 import random
 import math
 import operator
 
-def loadDataset(filename, split, trainingSet=[] , testSet=[]):
-	with open(filename, 'rb') as csvfile:
-	    lines = csv.reader(csvfile)
-	    dataset = list(lines)
-	    for x in range(len(dataset)-1):
-	        for y in range(4):
-	            dataset[x][y] = float(dataset[x][y])
-	        if random.random() < split:
-	            trainingSet.append(dataset[x])
-	        else:
-	            testSet.append(dataset[x])
+def load_data(file_path, split, trainingSet=[] , testSet=[]):
+    with open(file_path) as dataset:
+        header = dataset.readline()[:-1].split(',')
+        data = [line[:-1].split(',') for line in dataset]
+        for i in range(len(data)):
+            data[i] = list(map(float, data[i][:-1])) + [data[-1]]
 
+        for x in range(len(data)-1):
+            if random.random() < split:
+                trainingSet.append(data[x])
+            else:
+                testSet.append(data[x])
+
+    return trainingSet, testSet
 
 def euclideanDistance(instance1, instance2, length):
-	distance = 0
-	for x in range(length):
-		distance += pow((instance1[x] - instance2[x]), 2)
-	return math.sqrt(distance)
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x] - instance2[x]), 2)
+    return math.sqrt(distance)
 
 def getNeighbors(trainingSet, testInstance, k):
-	distances = []
-	length = len(testInstance)-1
-	for x in range(len(trainingSet)):
-		dist = euclideanDistance(testInstance, trainingSet[x], length)
-		distances.append((trainingSet[x], dist))
-	distances.sort(key=operator.itemgetter(1))
-	neighbors = []
-	for x in range(k):
-		neighbors.append(distances[x][0])
-	return neighbors
+    distances = []
+    length = len(testInstance)-1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
 
 def getResponse(neighbors):
-	classVotes = {}
-	for x in range(len(neighbors)):
-		response = neighbors[x][-1]
-		if response in classVotes:
-			classVotes[response] += 1
-		else:
-			classVotes[response] = 1
-	sortedVotes = sorted(iter(classVotes.items()), key=operator.itemgetter(1), reverse=True)
-	return sortedVotes[0][0]
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(iter(classVotes.items()), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
 
 def getAccuracy(testSet, predictions):
-	correct = 0
-	for x in range(len(testSet)):
-		if testSet[x][-1] == predictions[x]:
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0
-	
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == predictions[x]:
+            correct += 1
+    return (correct/float(len(testSet))) * 100.0
+    
 def main():
-	# prepare data
-	trainingSet=[]
-	testSet=[]
-	split = 0.67
-	loadDataset('dataset/iris_training.csv', split, trainingSet, testSet)
-	print('Train set: ' + repr(len(trainingSet)))
-	print('Test set: ' + repr(len(testSet)))
-	# generate predictions
-	predictions=[]
-	k = 3
-	for x in range(len(testSet)):
-		neighbors = getNeighbors(trainingSet, testSet[x], k)
-		result = getResponse(neighbors)
-		predictions.append(result)
-		print(('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1])))
-	accuracy = getAccuracy(testSet, predictions)
-	print(('Accuracy: ' + repr(accuracy) + '%'))
-	
+    # prepare data
+    trainingSet=[]
+    testSet=[]
+    split = 0.67
+    loadData('data/iris_classic.csv', split, trainingSet, testSet)
+    print('Train set: ' + repr(len(trainingSet)))
+    print('Test set: ' + repr(len(testSet)))
+    # generate predictions
+    predictions=[]
+    k = 3
+    for x in range(len(testSet)):
+        neighbors = getNeighbors(trainingSet, testSet[x], k)
+        result = getResponse(neighbors)
+        predictions.append(result)
+        print(('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1])))
+    accuracy = getAccuracy(testSet, predictions)
+    print(('Accuracy: ' + repr(accuracy) + '%'))
+    
 main()
