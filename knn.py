4e489e3d25da0bf165bbb902ebb263c0b094c591
williamabreu <williamsantos_00@hotmail.com>
2018-11-18 20:49:02 -0200

    Documentado o projeto

--- knn.py
+++ knn.py
@@ -1,4 +1,20 @@
-def load_dataset(file_path):
+# Implementação do algoritmo KNN
+# 
+# Funciona a partir de uma base de dados em que parte 
+# dela será usada para treinamento e o restante dos
+# dados serão usados para testar a taxa de reconhecimento
+# do sistema implementado.
+# 
+# A implementação está de maneira independente da instância,
+# assim qualquer problema KNN pode ser resolvido por ela,
+# desde que seja obedecida a formatação do arquivo CSV.
+
+
+
+def load_dataset(file_path: str) -> dict:
+    """
+    Carrega o arquivo CSV do dataset em JSON para o sistema
+    """
     classes = None
 
     with open(file_path) as dataset:
@@ -16,7 +32,11 @@ def load_dataset(file_path):
 
     return classes
 
-def split_dataset(data, rate):
+def split_dataset(data: dict, rate: float) -> (dict, dict):
+    """
+    Divide o dataset JSON em dois subconjuntos disjuntos, um
+    para treinar e o outro para testar o sistema
+    """
     training_data = {}
     testing_data = {}
     
@@ -28,10 +48,16 @@ def split_dataset(data, rate):
 
     return training_data, testing_data
 
-def euclidean_distance(dataline1, dataline2):
+def euclidean_distance(dataline1: list, dataline2: list) -> float:
+    """
+    Calcula a distância euclideana entre duas instâncias do dataset
+    """
     return sum([(x - y)**2 for x, y in zip(dataline1, dataline2)])
 
-def neighborhood(testing_instance, training_data, k):
+def neighborhood(testing_instance: list, training_data: dict, k: int) -> list:
+    """
+    Calcula os k vizinhos de uma instância do dataset
+    """
     distances = []
 
     for classname in training_data:
@@ -42,12 +68,19 @@ def neighborhood(testing_instance, training_data, k):
     distances.sort(key=lambda x: x['distance'])
     return distances[:k]
 
-def predict(neighbors):
+def predict(neighbors: list) -> str:
+    """
+    Calcula qual classe tem maior incidência nos vizinhos por
+    maioria simples
+    """
     classnames = [x['classname'] for x in neighbors]
     names_count = [{'classname': name, 'count': classnames.count(name)} for name in set(classnames)]
     return max(names_count, key=lambda x: x['count'])['classname']
     
-def KNN_run(dataset_path, split_rate, k):
+def KNN_run(dataset_path: str, split_rate: float, k: int) -> None:
+    """
+    Chamada principal do algoritmo KNN
+    """
     data = load_dataset(dataset_path)
     training_data, testing_data = split_dataset(data, split_rate)
     
@@ -71,6 +104,9 @@ def KNN_run(dataset_path, split_rate, k):
         print(confusion_matrix[key])
     
 if __name__ == '__main__':
+    # Uso pelo terminal:
+    # python3 knn.py <str: dataset.csv> <float: taxa_de_divisão> <int: k>
+
     from sys import argv
     dataset_path, split_rate, k = argv[1], float(argv[2]), int(argv[3])
     KNN_run(dataset_path, split_rate, k)
